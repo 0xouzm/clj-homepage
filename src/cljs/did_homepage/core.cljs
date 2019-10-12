3eed0849abebb953bcac785158ba9dc18eabcd9a
wujj <wujj@cpchain.io>
2019-10-12 17:25:23 +0800

    add slider

--- src/cljs/did_homepage/core.cljs
+++ src/cljs/did_homepage/core.cljs
@@ -23,9 +23,61 @@
     [tab-link "#/build-os-x" "Build on macOX" :osx]
     [tab-link "#/build-enterprise" "Self-hosted" :enterprise]]])
 
+(def ci-data (r/atom {:container-num 1 :parallelism-num 1}))
+
+(defn calc-price []
+  (let [{:keys [container-num price] :as data} @ci-data]
+    (if (nil? price)
+      (assoc data :price (* container-num 50)))))
+
+(defn slider [param value min max]
+  [:input {:type      "range" :value value :min min :max max
+           :style     {:width "100%"}
+           :on-change (fn [e]
+                        ;(println @ci-data)
+                        (swap! ci-data assoc param (.. e -target -value)))}])
+(defn concurrent-btn [parallelism-num]
+  [:p.control
+   [:a.button.is-rounded {
+                          :class    (when (= parallelism-num (:parallelism-num @ci-data)) :is-active)
+                          :on-click (fn [e]
+                                      (swap! ci-data assoc :parallelism-num parallelism-num))}
+
+
+
+    [:span (str parallelism-num "x")]]])
+
+(defn bmi-component []
+  (let [{:keys [container-num price]} (calc-price)]
+    [:div
+     [:h2 "Containers"]
+     [:p.subtitle "All of your containers are shared across your entire organization."]
+     [:div
+      [slider :container-num container-num 1 64]]
+     [:h2 "Parallelism"]
+     [:p "You can run " [:span.value container-num] " concurrent jobs with " [:span.value container-num] " containers " [:span.value (:parallelism-num @ci-data)] "x parallelism."]
+     ;[:p "price:" price]
+     ;[:p "p-num" (:parallelism-num @ci-data)]
+     [:div.field.has-addons {:style {:width "100%"}}
+      [concurrent-btn 1]
+      [concurrent-btn 2]
+      [concurrent-btn 8]
+      [concurrent-btn 12]
+      [concurrent-btn 16]]]))
+
+
+
 (defn linux-page []
   [:section.section>div.container>div.content
-   "1"])
+   [:div.has-text-centered
+    [:h3.title "The first container is free + each additional container is $50/month "] [:p.is-size-5 "Build on both Linux and macOS? " [:a "Start a free 2-week macOS trial which allows you to build
+     on both."]]]
+   [:div.columns>div.slider-box
+    [:div.column
+     [bmi-component]]
+
+    [:div.column]]])
+
 
 (defn price-item [is-featured]
   [:div.column.price-calculator-preview.is-marginless.is-paddingless.has-text-black-bis {:class (when (= "featured" is-featured) "featured")}
